b77da516b6adc920d26c5571e253615e9b62cf1c
zephyr005 <rajkumar.rbj20@gmail.com>
2023-01-28 19:05:36 +0530

    Code4

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -60,11 +60,24 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     @Override
     public Spot updateSpot(int parkingLotId, int spotId, int pricePerHour) {
         ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
-        Spot spot = spotRepository1.findById(spotId).get();
+//        Spot spot = spotRepository1.findById(spotId).get();
+//
+//        spot.setPricePerHour(pricePerHour);
+//        parkingLot.getSpotList().add(spot);
+//        parkingLotRepository1.save(parkingLot);
 
+        Spot spot = null;
+        List<Spot> spotList = parkingLot.getSpotList();
+        for(Spot spot1 : spotList){
+            if(spot1.getId() == spotId){
+                spot = spot1;
+            }
+        }
+
+        spot.setParkingLot(parkingLot);
         spot.setPricePerHour(pricePerHour);
-        parkingLot.getSpotList().add(spot);
-        parkingLotRepository1.save(parkingLot);
+
+        spotRepository1.save(spot);
 
         return spot;
     }
